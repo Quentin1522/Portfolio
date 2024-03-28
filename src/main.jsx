44f6e6c73aace961bed5bac5b69711cedbf1de99
Quentin Honreck <horneck.quentin@gmail.com>
2024-03-28 19:35:16 +0100

    maj

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import reportWebVitals from "./reportWebVitals";
-
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,4 +9,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
-reportWebVitals();
