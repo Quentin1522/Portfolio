da0684c70413b847e8f6a5de151fcffc8c7244eb
Quentin Honreck <horneck.quentin@gmail.com>
2024-03-29 11:20:51 +0100

    basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import "../src/App.scss";
+import process from 'process';
 
 function App() {
+    const basename = process.env.NODE_ENV === "production" ? "/Portfolio/" : "/";
+
     return (
         <div>
-            <BrowserRouter>
+            <BrowserRouter basename={basename}>
                 <Routes>
-                    <Route path="/Portfolio/" element={<Home />} />
+                    <Route path="/" element={<Home />} />
                 </Routes>
             </BrowserRouter>
         </div>
